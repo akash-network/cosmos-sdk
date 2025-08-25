138c57041d8ed3258c8ddfaaf1dc6f3ba1d86673
Artur Troian <troian@users.noreply.github.com>
2025-08-24 21:14:52 -0500

    feat: aep-75
    Signed-off-by: Artur Troian <troian@users.noreply.github.com>

--- x/authz/simulation/decoder.go
+++ x/authz/simulation/decoder.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/types/kv"
 	"github.com/cosmos/cosmos-sdk/x/authz"
-	"github.com/cosmos/cosmos-sdk/x/authz/keeper"
+	"github.com/cosmos/cosmos-sdk/x/authz/keeper/keys"
 )
 
 // NewDecodeStore returns a decoder function closure that umarshals the KVPair's
@@ -15,12 +15,12 @@ import (
 func NewDecodeStore(cdc codec.Codec) func(kvA, kvB kv.Pair) string {
 	return func(kvA, kvB kv.Pair) string {
 		switch {
-		case bytes.Equal(kvA.Key[:1], keeper.GrantKey):
+		case bytes.Equal(kvA.Key[:1], keys.GrantKey):
 			var grantA, grantB authz.Grant
 			cdc.MustUnmarshal(kvA.Value, &grantA)
 			cdc.MustUnmarshal(kvB.Value, &grantB)
 			return fmt.Sprintf("%v\n%v", grantA, grantB)
-		case bytes.Equal(kvA.Key[:1], keeper.GrantQueuePrefix):
+		case bytes.Equal(kvA.Key[:1], keys.GrantQueuePrefix):
 			var grantA, grantB authz.GrantQueueItem
 			cdc.MustUnmarshal(kvA.Value, &grantA)
 			cdc.MustUnmarshal(kvB.Value, &grantB)
