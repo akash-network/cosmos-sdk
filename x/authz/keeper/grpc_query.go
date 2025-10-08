7d7d27c8f09c74a5857ce21d67497f5e7a4b1788
Artur Troian <troian@users.noreply.github.com>
2025-10-07 21:47:02 -0500

    feat(x/authz): implement custom pagination encoder for grantee queries
    Signed-off-by: Artur Troian <troian@users.noreply.github.com>

--- x/authz/keeper/grpc_query.go
+++ x/authz/keeper/grpc_query.go
@@ -2,12 +2,14 @@ package keeper
 
 import (
 	"context"
+	"encoding/binary"
+	"hash/crc32"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	"cosmossdk.io/errors"
-	"cosmossdk.io/store/prefix"
+	prefixstore "cosmossdk.io/store/prefix"
 
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/runtime"
@@ -68,7 +70,7 @@ func (k Keeper) Grants(ctx context.Context, req *authz.QueryGrantsRequest) (*aut
 
 	store := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
 	key := keys.GrantStoreKey(grantee, granter, "")
-	grantsStore := prefix.NewStore(store, key)
+	grantsStore := prefixstore.NewStore(store, key)
 
 	authorizations, pageRes, err := query.GenericFilteredPaginate(k.cdc, grantsStore, req.Pagination, func(key []byte, auth *authz.Grant) (*authz.Grant, error) {
 		auth1, err := auth.GetAuthorization()
@@ -117,7 +119,7 @@ func (k Keeper) GranterGrants(ctx context.Context, req *authz.QueryGranterGrants
 	}
 
 	store := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
-	authzStore := prefix.NewStore(store, keys.GrantStoreKey(nil, granter, ""))
+	authzStore := prefixstore.NewStore(store, keys.GrantStoreKey(nil, granter, ""))
 
 	grants, pageRes, err := query.GenericFilteredPaginate(k.cdc, authzStore, req.Pagination, func(key []byte, auth *authz.Grant) (*authz.GrantAuthorization, error) {
 		auth1, err := auth.GetAuthorization()
@@ -150,6 +152,92 @@ func (k Keeper) GranterGrants(ctx context.Context, req *authz.QueryGranterGrants
 	}, nil
 }
 
+// decodePaginationKey decodes a pagination key into a prefix and primary key or returns an error if the key is invalid.
+func decodePaginationKey(key []byte) ([]byte, []byte, error) {
+	var prefix []byte
+	var pKey []byte
+
+	if len(key) > 0 {
+		if len(key) < 5 {
+			return nil, nil, errors.Wrapf(query.ErrInvalidPaginationKey, "invalid key length")
+		}
+
+		expectedChecksum := binary.BigEndian.Uint32(key)
+
+		key = key[4:]
+
+		checksum := crc32.ChecksumIEEE(key)
+
+		if expectedChecksum != checksum {
+			return nil, nil, errors.Wrapf(query.ErrInvalidPaginationKey, "invalid checksum, 0x%08x != 0x%08x", expectedChecksum, checksum)
+		}
+
+		for len(key) > 0 {
+			if len(key) < 2 {
+				return nil, nil, errors.Wrapf(query.ErrInvalidPaginationKey, "invalid key length")
+			}
+			keyType := key[0]
+			key = key[1:]
+			prefixLength := int(key[0])
+			key = key[1:]
+
+			if len(key) < prefixLength {
+				return nil, nil, errors.Wrapf(query.ErrInvalidPaginationKey, "invalid key length")
+			}
+
+			switch keyType {
+			case 1:
+				prefix = make([]byte, prefixLength)
+				copy(prefix, key[:prefixLength])
+			case 2:
+				pKey = make([]byte, prefixLength)
+				copy(pKey, key[:prefixLength])
+			default:
+				return nil, nil, errors.Wrapf(query.ErrInvalidPaginationKey, "invalid key type")
+			}
+
+			key = key[prefixLength:]
+		}
+	}
+
+	return prefix, pKey, nil
+}
+
+// encodePaginationKey creates a unique byte key by encoding a prefix and key with a checksum for validation.
+// Returns nil if both prefix and key inputs are nil.
+func encodePaginationKey(prefix []byte, key []byte) []byte {
+	if prefix == nil && key == nil {
+		return nil
+	}
+
+	// checksum
+	encLen := 4 + 1 + 1 + 1 + 1 + len(prefix) + len(key)
+
+	buf := make([]byte, encLen)
+
+	data := buf[4:]
+
+	data[0] = 1
+	dLen := uint8(len(prefix))
+	data[1] = dLen
+	data = data[2:]
+	copy(data, prefix)
+	data = data[dLen:]
+
+	data[0] = 2
+
+	dLen = uint8(len(key))
+	data[1] = dLen
+	data = data[2:]
+	copy(data, key)
+	data = data[dLen:]
+
+	checksum := crc32.ChecksumIEEE(buf[4:])
+	binary.BigEndian.PutUint32(buf, checksum)
+
+	return buf
+}
+
 // GranteeGrants implements the Query/GranteeGrants gRPC method.
 func (k Keeper) GranteeGrants(ctx context.Context, req *authz.QueryGranteeGrantsRequest) (*authz.QueryGranteeGrantsResponse, error) {
 	if req == nil {
@@ -164,26 +252,30 @@ func (k Keeper) GranteeGrants(ctx context.Context, req *authz.QueryGranteeGrants
 		req.Pagination.Limit = query.DefaultLimit
 	}
 
-	storePrefix := keys.GranteeGranterKey
+	storePrefix, pKey, err := decodePaginationKey(req.Pagination.Key)
+	if err != nil {
+		return nil, err
+	}
 
-	if len(req.Pagination.Key) == 0 {
-		grantee, err := k.authKeeper.AddressCodec().StringToBytes(req.Grantee)
-		if err != nil {
-			return nil, err
+	if len(storePrefix) == 0 {
+		storePrefix = keys.GranteeGranterKey
+		if req.Grantee != "" {
+			grantee, err := k.authKeeper.AddressCodec().StringToBytes(req.Grantee)
+			if err != nil {
+				return nil, err
+			}
+
+			storePrefix = keys.GranteeGranterStoreKey(grantee, nil, "")
 		}
-
-		storePrefix = keys.GranteeGranterStoreKey(grantee, nil, "")
+	} else {
+		req.Pagination.Key = pKey
 	}
 
 	var grants []*authz.GrantAuthorization
 
-	gStore := prefix.NewStore(runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx)), storePrefix)
+	gStore := prefixstore.NewStore(runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx)), storePrefix)
 
 	pageRes, err := query.FilteredPaginate(gStore, req.Pagination, func(key []byte, value []byte, accumulate bool) (bool, error) {
-		if !accumulate {
-			return false, nil
-		}
-
 		grantee, granter, msgTypeUrl := keys.ParseGranteeGranterStoreKey(append(storePrefix, key...))
 
 		grant, found := k.getGrant(ctx, keys.GrantStoreKey(grantee, granter, msgTypeUrl))
@@ -201,18 +293,23 @@ func (k Keeper) GranteeGrants(ctx context.Context, req *authz.QueryGranteeGrants
 			return false, status.Errorf(codes.Internal, er.Error())
 		}
 
-		grants = append(grants, &authz.GrantAuthorization{
-			Authorization: authorizationAny,
-			Expiration:    grant.Expiration,
-			Granter:       granter.String(),
-			Grantee:       grantee.String(),
-		})
+		if accumulate {
+			grants = append(grants, &authz.GrantAuthorization{
+				Authorization: authorizationAny,
+				Expiration:    grant.Expiration,
+				Granter:       granter.String(),
+				Grantee:       grantee.String(),
+			})
+		}
 
 		return true, nil
 	})
-
 	if err != nil {
 		return nil, err
+	}
+
+	if len(pageRes.NextKey) > 0 {
+		pageRes.NextKey = encodePaginationKey(storePrefix, pageRes.NextKey)
 	}
 
 	return &authz.QueryGranteeGrantsResponse{
